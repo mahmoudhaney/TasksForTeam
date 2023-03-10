b6afee3f9b2dde58032f2e4a0d8b790fa9123476
mostafa <mostafa.gamal2002o@gmail.com>
2023-03-10 19:57:49 +0200

    apply changes

--- Mustafa/mostafaSolution1.cpp
+++ Mustafa/mostafaSolution1.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
-#include <math.h>
-#include <cmath>
 #include <string.h>
-#include<iomanip>
-#include <cstdlib>
-#include <algorithm>
-#include <cctype>
-#include <bits/stdc++.h>
-#include <cstdlib>
-#include <cstring>
-#include <vector>
-#include <iterator>
-#include <cstring>
-#include <list>
-#include <deque>
-#include <forward_list>
-#include <map>
-#include <stack>
-#include <queue>
-#include <unordered_map>
-#include <unordered_set>
 #include <set>
-#include <utility>
 using namespace std;
-#define long long int ll
-
-
 /*
     The problem is to check if the number of distinct characters is even or odd
         -if it is even then the answer is "chat with her " which means it's a girl
@@ -37,11 +13,11 @@ using namespace std;
 int main(){
     string name;
     cin>>name;
-    set<char> st; // we use set because the (set) remove duplicate characters
+    set<char> distinctCharacters ; // we use set because the (set) remove duplicate characters
     for(int i=0;i<name.size();i++){
-        st.insert(name[i]);
+        distinctCharacters.insert(name[i]);
     }
-    if(st.size()%2==0){
+    if(distinctCharacters.size()%2==0){
         cout<<"CHAT WITH HER!";
     }
     else{
--- Mustafa/mostafaSolution2.cpp
+++ Mustafa/mostafaSolution2.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
-#include <math.h>
-#include <cmath>
 #include <string.h>
-#include<iomanip>
-#include <cstdlib>
 #include <algorithm>
-#include <cctype>
-#include <bits/stdc++.h>
-#include <cstdlib>
-#include <cstring>
-#include <vector>
-#include <iterator>
-#include <cstring>
-#include <list>
-#include <deque>
-#include <forward_list>
-#include <map>
-#include <stack>
-#include <queue>
-#include <unordered_map>
-#include <unordered_set>
-#include <set>
-#include <utility>
 using namespace std;
-#define long long int ll
 
 
 /*
@@ -38,17 +16,17 @@ using namespace std;
 int main(){
 
     
-    int counter=0; //count the number of characters in name
+    int distinctCharacters=0; //count the number of characters in name
     string name;
     cin>>name;
     sort(name.begin(),name.end());//sort the characters to eliminate the duplicate char
     for(int i=0;i<name.size();i++){
         //if name at index i not equal name at index i+1 that means they are distinct
         if(name[i]!=name[i+1]){
-            counter++;
+            distinctCharacters++;
         }
     }
-    if(counter%2==0){
+    if(distinctCharacters%2==0){
         cout<<"CHAT WITH HER!";
     }
     else{
