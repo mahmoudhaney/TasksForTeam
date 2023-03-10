8a03b29906736875899a7cdd75dfc323479c4965
fayrouz <fayrouzgoamaa@gmail.com>
2023-03-10 20:51:30 +0200

    Edited

--- Fayrouz/solution.cpp
+++ Fayrouz/solution.cpp
@@ -5,16 +5,17 @@ using namespace std;
 int main()
 {
     int number_of_problems;
-    cin >> number_of_problems;
+    cin >> number_of_problems ;
     int implemented_problems = 0;
     while(number_of_problems)
     {
-        int Petya_veiw,Vasya_veiw,Tonya_veiw;
-        cin>>Petya_veiw>>Vasya_veiw>>Tonya_veiw;
-        if (Petya_veiw+Vasya_veiw==2||Vasya_veiw+Tonya_veiw==2||Petya_veiw+Tonya_veiw==2)
-            implemented_problems++ ;
-        number_of_problems--;
+        int Petya_veiw , Vasya_veiw , Tonya_veiw ;
+        cin>>Petya_veiw >> Vasya_veiw >> Tonya_veiw ;
+        int sum = Petya_veiw + Vasya_veiw + Tonya_veiw ;
+        if( sum >= 2)
+            implemented_problems ++ ;
+        number_of_problems -- ;
     }
-    cout<<implemented_problems;
+    cout << implemented_problems ;
     return 0;
 }
