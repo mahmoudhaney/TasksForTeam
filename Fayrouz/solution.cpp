a7827136392abdfe92a5fc2b406fecea4f974e01
fayrouz <fayrouzgoamaa@gmail.com>
2023-03-10 18:10:13 +0200

    Solved

--- Fayrouz/solution.cpp
+++ Fayrouz/solution.cpp
@@ -12,7 +12,7 @@ int main()
         int Petya_veiw,Vasya_veiw,Tonya_veiw;
         cin>>Petya_veiw>>Vasya_veiw>>Tonya_veiw;
         if (Petya_veiw+Vasya_veiw==2||Vasya_veiw+Tonya_veiw==2||Petya_veiw+Tonya_veiw==2)
-            implemented_problems++;
+            implemented_problems++ ;
         number_of_problems--;
     }
     cout<<implemented_problems;
